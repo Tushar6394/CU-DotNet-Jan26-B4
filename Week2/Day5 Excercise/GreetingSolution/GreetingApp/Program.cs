54b88fa7525147369956d057ccd694b70a072d32
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-01-12 12:48:36 +0530

    Done

---
+++ Week2/Day5 Excercise/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using GreetingLibrary;
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+string message = GreetingHelper.GetGreeting(name);
+
+Console.WriteLine(message);
