f2ec4ebc2bd3c0758c6290a8cbee4a136eed4238
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-01-13 11:08:53 +0530

    Remove nested git repo and add CS_Learning normally

---
+++ CS_Learning/Demo1DataTypes/Program.cs
@@ -0,0 +1,57 @@
+﻿/*class demo1DataTypes
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Demo of Data Types in C#");
+    }
+}
+
+//Boxing 
+class demo2Boxing
+{
+    public static void Main(string[] args)
+    {
+        int num = 123;
+        object obj = num;
+        Console.WriteLine("Boxed value: " + obj);
+    }
+}
+*/
+
+//modify the code and display the name also above the age get the age of the person in a single line and display in the single line the age and message you are able to code or not above the age 18 or below the age 18 and also //Display odd number from 1 to the age in a single line with spacing and dont use for loop for displaying odd numbers and dont show the sepearate line for name and age only Enter your name and age this is enough
+using System;
+class demo3UserInput
+{
+    public static void main(string[] args)
+    {
+        Console.WriteLine("Enter your name and age:");
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+        string name = parts[0];
+        int age = int.Parse(parts[1]);
+
+        Console.WriteLine($"Name: {name}, Age: {age}");
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are able to code.");
+        }
+        else
+        {
+            Console.WriteLine("You are not able to code.");
+        }
+
+        Console.Write("Odd numbers from 1 to " + age + ": ");
+        PrintOddNumbers(age, 1);
+    }
+
+    static void PrintOddNumbers(int max, int current)
+    {
+        if (current > max) return;
+        if (current % 2 != 0)
+        {
+            Console.Write(current + " ");
+        }
+        PrintOddNumbers(max, current + 1);
+    }
+}
---
+++ CS_Learning/Demo2OutputFormatting/Program.cs
@@ -0,0 +1,23 @@
+﻿//write a program to print table like 15*1=15 15*2=30 and make them pick there number from the user
+/*using System;
+namespace CS_Learning
+{
+    class demo2OutputFormatting
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to print its multiplication table:");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication Table for {number}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+        }
+    }
+}*/
+string name = "Tushar Singh";
+int value = 12345;
+Console.WriteLine("Name: {0}, Value: {1}", name, value);
