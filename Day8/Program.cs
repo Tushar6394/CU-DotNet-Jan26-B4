4edd0c034f0dabce0ef0fc73a1f08e1d3709e90f
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-01-14 10:05:15 +0530

    Done

---
+++ Day8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write(i + " ");
+            Console.Write((6 - i) + " ");
+        }
+        Console.WriteLine();
+    }
+}
