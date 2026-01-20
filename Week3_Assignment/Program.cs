cc618aea6ee6df114d684d690c01392cfa8f0e91
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-01-20 15:18:55 +0530

    Done

---
+++ Week3_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿//Example of Method and also this is a Week3 assessment on Top Brains 
+public class Solution{
+public static double CalculateGymBill(bool tread, bool weight, bool zumba)
+{
+    double amt=1000;
+    if(!tread && !weight && !zumba)
+    {
+        amt+=200;
+    }
+    else{
+        amt+=tread?300:0;
+        amt+=weight?500:0;
+        amt+=zumba?250:0;
+    }
+
+    return (amt+amt*0.05);
+}
+
+}
