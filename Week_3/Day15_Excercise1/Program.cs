967ccb48d3f35d68b6af0548f4e8b563417bb024
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-01-22 16:47:21 +0530

    Day15 Excercise 1 Done

---
+++ Week_3/Day15_Excercise1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Height
+{
+    public int Feet;
+    public double Inches;
+
+    // Default constructor
+    public Height()
+    {
+        Feet = 0;
+        Inches = 0.0;
+    }
+
+    // Parameterized constructor
+    public Height(int feet, double inches)
+    {
+        Feet = feet;
+        Inches = inches;
+    }
+
+    // Add heights and return new Height
+    public Height AddHeights(Height h2)
+    {
+        double totalInches = Inches + h2.Inches;
+        int totalFeet = Feet + h2.Feet + (int)(totalInches / 12);
+        double newInches = totalInches % 12;
+        return new Height(totalFeet, newInches);
+    }
+
+    public override string ToString()
+    {
+        return $"Height - {Feet} feet {Inches:F1} inches";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Height p1 = new Height(5, 6.5);
+        Height p2 = new Height(5, 7.5);
+        
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p1.AddHeights(p2));
+    }
+}
