da8b256d504f0694c6a49d6dad609ee6d85cc215
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-02-15 23:13:33 +0530

    Done

--- Day33/Program.cs
+++ Day33/Program.cs
@@ -1,4 +1,81 @@
-﻿using System;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// class Person
+// {
+//     public string Name;
+//     private List<Person> friends = new List<Person>();
+
+//     public Person(string name)
+//     {
+//         Name = name;
+//     }
+
+//     // Getter method to access friends
+//     public List<Person> GetFriends()
+//     {
+//         return friends;
+//     }
+
+//     // Add friend (undirected graph logic)
+//     public void AddFriend(Person friend)
+//     {
+//         // Prevent null or self-friendship
+//         if (friend == null || friend == this)
+//             return;
+
+//         // Prevent duplicate friendship
+//         if (!friends.Contains(friend))
+//         {
+//             friends.Add(friend);
+//             friend.friends.Add(this);
+//         }
+//     }
+// }
+
+// class SocialNetwork
+// {
+//     private List<Person> members = new List<Person>();
+
+//     public void AddMember(Person person)
+//     {
+//         members.Add(person);
+//     }
+
+//     public void ShowNetwork()
+//     {
+//         foreach (var person in members)
+//         {
+//             string friendNames = string.Join(", ",
+//                 person.GetFriends().Select(f => f.Name));
+
+//             Console.WriteLine(person.Name + "-> " + friendNames);
+//         }
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Person tushar = new Person("Tushar");
+//         Person sahil = new Person("Sahil");
+//         Person aman = new Person("Aman");
+
+//         SocialNetwork network = new SocialNetwork();
+
+//         network.AddMember(tushar);
+//         network.AddMember(sahil);
+//         network.AddMember(aman);
+
+//         tushar.AddFriend(sahil);
+//         sahil.AddFriend(aman);
+
+//         network.ShowNetwork();
+//     }
+// }
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
