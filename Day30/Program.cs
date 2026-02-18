28424455222eb4a8082d4cefd983fa1427feebbb
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-02-18 16:09:12 +0530

    Linq Done

--- Day30/Program.cs
+++ Day30/Program.cs
@@ -68,29 +68,110 @@
 // }
 
 ///////////////Delegate////////////////////
-using System.Data;
-using System.Diagnostics;
-using System.Net.NetworkInformation;
-using System.Reflection.Metadata;
-using System.Runtime.CompilerServices;
+// namespace Week6Learning
+// {
+//     delegate void MyDelegate();
+//     internal class Demo02Delegates
+//     {
+//         static void MyMethod1()
+//         {
+//             Console.WriteLine("MyMethod1");
+//         }
+//         static void MyMethod2()
+//         {
+//             Console.WriteLine("MyMethod2");
+//         }
+//         static void Main (string [] args)
+//         {
+//             MyDelegate del1 = MyMethod1;
+//             del1 += MyMethod2;
+//             del1();
+//             Console.WriteLine("---------");
+//             del1 -= MyMethod1;
+//             del1();
+//         }
+//     }
+// }
 
+
+// namespace Week6Learning
+// {
+//     delegate void MyDelegate();//because we want refer to an anonymous method, we need to create a delegate type
+//     internal class Demo03DelegateLambda
+//     {
+//         static void MyMethod1()
+//         {
+//             Console.WriteLine("MyMethod1");
+//         }
+//         static void MyMethod2()
+//         {
+//             Console.WriteLine("MyMethod2");
+//         }
+//         static void Main(string[] args)
+//         {
+//             MyDelegate del1 = delegate ()
+//             {
+//                 Console.WriteLine("Anonymous method referred..");
+//             };
+//             del1();
+//             //Lambda expression is a replacement for delegate and anonymous method.
+//             MyDelegate del2 = () => Console.WriteLine("Lambda expression referred..");
+
+//             Action act1 = () => Console.WriteLine("Action working...");
+
+//             Action<int> act2 = (x) => Console.WriteLine("Action Working - " + x);
+
+//             act2(10);
+
+//             Action<int, string> act3 = (num, name) => Console.WriteLine($"Action Working - {num} {name}");
+
+//             act3(20, "Hello");
+
+//             Action<int, string> act4 = (num, name) => 
+//             {
+//                 Console.WriteLine($"Action Working - {num}");
+//                 Console.WriteLine($"Action Working - {name}");
+//             };
+//             act4(30, "World");
+
+//             Func<int,int> GetDouble = (x) => x * 2;
+
+//             int result1 = GetDouble(5);
+//             Console.WriteLine(result1);
+
+//             Func<int,int, int> Getsum = (x, y) => x + y;
+//         }
+
+//     }
+// }
+
+
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows.Markup;
+using Microsoft.VisualBasic;
+
+///Action and Func Used.
 namespace Week6Learning
 {
-    delegate void MyDelegate();
-    internal class Demo02Delegates
+    internal class Demo04LINQ
     {
-        static void MyMethod1()
+        static void Main(string[] args)
         {
-            Console.WriteLine("MyMethod1");
-        }
-        static void MyMethod2()
-        {
-            Console.WriteLine("MyMethod2");
-        }
-        static void Main (string [] args)
-        {
-            MyDelegate del1 = MyMethod1;
-            del1();
+            List<int> values = new List<int>
+            {
+                12,13,45,46,67,68,58,59
+            };
+            Console.WriteLine(values.Count());
+
+            var abovefifty = values.Where(x => x > 50).OrderByDescending(x => x);
+
+            Console.WriteLine(string.Join(", ", abovefifty));
+
+            List<int> abovefiftyList = values.Where(x => x > 50).ToList();
+
+            Console.WriteLine(string.Join(", ", abovefiftyList));
         }
     }
 }
