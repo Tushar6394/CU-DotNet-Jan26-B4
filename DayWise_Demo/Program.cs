81a2a7eed7cac267b5e9a85f88899a84bbb39d12
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-02-18 16:07:52 +0530

    Interface Segregation Principle Example Added

--- DayWise_Demo/Program.cs
+++ DayWise_Demo/Program.cs
@@ -189,25 +189,172 @@
 // }
 
 // pattern printing 1's and 0's( 1's in diagonals and 0's in other places)
-internal class Program
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int n = 5;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             for (int j = 1; j <= n; j++)
+//             {
+//                 if (i == j || j == (n - i + 1))
+//                 {
+//                     Console.Write("1 ");
+//                 }
+//                 else
+//                 {
+//                     Console.Write("0 ");
+//                 }
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+// Write a simple factorial program using recursion and also print all the numbers and print the values of n also in the output
+//print in this format like 
+//5
+////3
+/////2
+//////1
+//////2
+/////6
+////24
+//120 
+// using System;
+
+// class Program
+// {
+//     static int Fact(int n, int space)
+//     {
+//         for (int i = 0; i < space; i++)
+//             Console.Write("  ");
+
+//         Console.WriteLine(n);
+
+//         if (n == 1)
+//             return 1;
+
+//         int result = n * Fact(n - 1, space + 1);
+//         for (int i = 0; i < space; i++)
+//             Console.Write("  ");
+
+//         Console.WriteLine(result);
+
+//         return result;
+//     }
+
+//     static void Main()
+//     {
+//         int n = 5;
+//         Fact(n, 0);
+//     }
+// }
+
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using System.Xml;
+
+// namespace ClassPractice
+// {
+//     interface IDevice
+//     {
+//         void Print();
+//     }
+//     class Printer: IDevice 
+//     {
+//         public void Print()
+//         {
+//             Console.WriteLine("Printing");
+//         }
+//     }
+//     class InkjetPrinter: IDevice
+//     {
+//         public void Print()
+//         {
+//             Console.WriteLine("InkjetPrinter");
+//         }
+//     }
+//     class Computer
+//     {
+//         private IDevice device;
+
+//         //private Printer p = new Printer();
+//         public Computer(IDevice d)
+//         {
+//             device = d;
+//         }
+
+//         public void StartPrinting()
+//         {
+//             device.Print();
+//             //p.Print();
+//         }
+//     }
+//     internal class DIP
+//     {
+//         static void Main(string[] args)
+//         {
+//             IDevice device = new InkjetPrinter();
+//             IDevice device1 = new Printer();
+//             Computer c = new Computer(device);
+//             Computer c1 = new Computer(device1);
+//             //Computer c = new Computer();
+//             c.StartPrinting();
+//             c1.StartPrinting();
+//         }
+//     }
+// }
+
+
+///////////////Interface Segregation Principle Example //////////////////////////
+using System;
+interface IPrinter
 {
-    private static void Main(string[] args)
+    void Print();
+}
+interface IScanner
+{
+    void Scan();
+}
+class SimplePrinter : IPrinter
+{
+    public void Print()
     {
-        int n = 5;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if (i == j || j == (n - i + 1))
-                {
-                    Console.Write("1 ");
-                }
-                else
-                {
-                    Console.Write("0 ");
-                }
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine("Simple Printer: Printing...");
+    }
+}
+
+class MultiFunctionPrinter : IPrinter, IScanner
+{
+    public void Print()
+    {
+        Console.WriteLine("MultiFunction Printer: Printing...");
+    }
+
+    public void Scan()
+    {
+        Console.WriteLine("MultiFunction Printer: Scanning...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SimplePrinter sp = new SimplePrinter();
+        sp.Print();
+
+        Console.WriteLine();
+
+        MultiFunctionPrinter mp = new MultiFunctionPrinter();
+        mp.Print();
+        mp.Scan();
     }
 }
