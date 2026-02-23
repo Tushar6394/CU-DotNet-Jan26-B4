dbbd750b0e6daaf3e9ba596e2110ac70ad8be2d2
Tushar6394 <tusharsingh06.ts@gmail.com>
2026-02-23 16:05:08 +0530

    Practice Done

--- DayWise_Demo/Program.cs
+++ DayWise_Demo/Program.cs
@@ -361,56 +361,300 @@
 
 
 
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+// namespace CommonInsertion_Sort
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             // Create an array of integers for sorting
+//             int[] numbers = new int[10] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+//             // Display original array elements
+//             Console.WriteLine("\nOriginal Array Elements :");
+//             PrintIntegerArray(numbers);
+
+//             // Perform Insertion Sort and display the sorted array elements
+//             Console.WriteLine("\nSorted Array Elements :");
+//             PrintIntegerArray(InsertionSort(numbers));
+//             Console.WriteLine("\n");
+//         }
+
+//         // Method implementing Insertion Sort algorithm
+//         static int[] InsertionSort(int[] inputArray)
+//         {
+//             for (int i = 0; i < inputArray.Length - 1; i++)
+//             {
+//                 for (int j = i + 1; j > 0; j--)
+//                 {
+//                     // Swap if the element at j - 1 position is greater than the element at j position
+//                     if (inputArray[j - 1] > inputArray[j])
+//                     {
+//                         int temp = inputArray[j - 1];
+//                         inputArray[j - 1] = inputArray[j];
+//                         inputArray[j] = temp;
+//                     }
+//                 }
+//             }
+//             return inputArray; // Return the sorted array
+//         }
+
+//         // Method to print integer array elements
+//         public static void PrintIntegerArray(int[] array)
+//         {
+//             foreach (int i in array)
+//             {
+//                 Console.Write(i.ToString() + "  "); // Display each element followed by a space
+//             }
+//         }
+//     }
+// }
+
+
+// class Loan
+// {
+//     public string LoanNumber { get; set; }
+//     public string CustomerName { get; set; }
+//     public decimal PrincipalAmount { get; set; }
+//     public int TenureInYears { get; set; }
+
+//     public Loan(string loanNumber, string customerName, decimal principalAmount, int tenureInYears)
+//     {
+//         LoanNumber = loanNumber;
+//         CustomerName = customerName;
+//         PrincipalAmount = principalAmount;
+//         TenureInYears = tenureInYears;
+//     }
+
+//     public decimal CalculateEMI()
+//     {
+//         decimal interestRate = 0.10m;
+//         decimal totalAmount = PrincipalAmount + (PrincipalAmount * interestRate * TenureInYears);
+//         decimal emi = totalAmount / (TenureInYears * 12);
+//         return emi;
+//     }
+// }
+
+// class HomeLoan : Loan
+// {
+//     public HomeLoan(string loanNumber, string customerName, decimal principalAmount, int tenureInYears)
+//         : base(loanNumber, customerName, principalAmount, tenureInYears)
+//     {
+//     }
+
+//     public new decimal CalculateEMI()
+//     {
+//         decimal interestRate = 0.08m;
+//         decimal processingFee = PrincipalAmount * 0.01m;
+//         decimal totalAmount = PrincipalAmount + processingFee + (PrincipalAmount * interestRate * TenureInYears);
+//         decimal emi = totalAmount / (TenureInYears * 12);
+//         return emi;
+//     }
+// }
+
+// class CarLoan : Loan
+// {
+//     public CarLoan(string loanNumber, string customerName, decimal principalAmount, int tenureInYears)
+//         : base(loanNumber, customerName, principalAmount, tenureInYears)
+//     {
+//     }
+
+//     public new decimal CalculateEMI()
+//     {
+//         decimal interestRate = 0.09m;
+//         decimal insuranceCharge = 15000m;
+//         decimal totalAmount = PrincipalAmount + insuranceCharge + (PrincipalAmount * interestRate * TenureInYears);
+//         decimal emi = totalAmount / (TenureInYears * 12);
+//         return emi;
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         HomeLoan h1 = new HomeLoan("H01", "A", 500000m, 20);
+//         HomeLoan h2 = new HomeLoan("H02", "B", 300000m, 15);
+//         CarLoan c1 = new CarLoan("C01", "C", 80000m, 5);
+//         CarLoan c2 = new CarLoan("C02", "D", 60000m, 4);
+
+//         Console.WriteLine($"Home Loan EMI: {h1.CalculateEMI():C}");
+//         Console.WriteLine($"Home Loan EMI: {h2.CalculateEMI():C}");
+//         Console.WriteLine($"Car Loan EMI: {c1.CalculateEMI():C}");
+//         Console.WriteLine($"Car Loan EMI: {c2.CalculateEMI():C}");
+//     }
+// }
+
+
+// using System;
+
+// abstract class Vehicle
+// {
+//     public string ModelName { get; set; }
+
+//     public abstract void Move();
+//     public virtual string GetFuelStatus() => "Fuel level is stable.";
+// }
+
+// class ElectricCar : Vehicle
+// {
+//     public override void Move() =>
+//         Console.WriteLine($"{ModelName} is running.");
+
+//     public override string GetFuelStatus() =>
+//         $"{ModelName} battery is at 80%.";
+// }
+
+// class HeavyTruck : Vehicle
+// {
+//     public override void Move() =>
+//         Console.WriteLine($"{ModelName} is hauling cargo.");
+// }
+
+// class CargoPlane : Vehicle
+// {
+//     public override void Move() =>
+//         Console.WriteLine($"{ModelName} is ascending to 30,000 feet.");
+
+//     public override string GetFuelStatus() =>
+//         base.GetFuelStatus() + " Checking jet fuel reserves...";
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Vehicle[] fleet =
+//         {
+//             new ElectricCar { ModelName = "Tesla Model B" },
+//             new HeavyTruck  { ModelName = "VolvoA "   },
+//             new CargoPlane  { ModelName = "BoeingC"  }
+//         };
+
+//         foreach (Vehicle vehicle in fleet)
+//         {
+//             vehicle.Move();
+//             Console.WriteLine(vehicle.GetFuelStatus());
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+
+// using System;
+
+// internal class BankAccount
+// {
+//     public string AccountHolder = "Tushar";
+//     private double Balance = 5000;
+//     protected string AccountType = "Savings";
+
+//     public void ShowBalance()
+//     {
+//         Console.WriteLine("Balance: " + Balance);
+//     }
+// } 
+// using System;
+// class Student
+// {
+//     public string Name;
+//     public int Age;
+//     public Student()
+//     {
+//         Name = "Unknown";
+//         Age = 0;
+//         Console.WriteLine("Default Constructor Called");
+//     }
+//     public Student(string name, int age)
+//     {
+//         Name = name;
+//         Age = age;
+//         Console.WriteLine("Parameterized Constructor Called");
+//     }
+//     private Student(string name)
+//     {
+//         Name = name;
+//         Console.WriteLine("Private Constructor Called");
+//     }
+
+//     public void Display()
+//     {
+//         Console.WriteLine("Name: " + Name + ", Age: " + Age);
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Student s1 = new Student();
+//         s1.Display();
+
+//         Console.WriteLine();
+
+//         Student s2 = new Student("Tushar", 21);
+//         s2.Display();
+
+//     }
+// }
+
+
+
+
+// using System;
+
+// class Student
+// {
+//     public string Name;
+
+//     // Constructor
+//     public Student(string name)
+//     {
+//         Name = name;
+//         PrintMessage();  
+//     }
+
+//     public void PrintMessage()
+//     {
+//         Console.WriteLine("Student Created: " + Name);
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Student s = new Student("Tushar");
+//     }
+// }
+
+
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace CommonInsertion_Sort
+class Calculator
 {
-    class Program
+    public void CalculateArea()
     {
-        static void Main(string[] args)
-        {
-            // Create an array of integers for sorting
-            int[] numbers = new int[10] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+        const int Length = 10;
+        const int Width = 5;
 
-            // Display original array elements
-            Console.WriteLine("\nOriginal Array Elements :");
-            PrintIntegerArray(numbers);
+        int area = Length * Width;
 
-            // Perform Insertion Sort and display the sorted array elements
-            Console.WriteLine("\nSorted Array Elements :");
-            PrintIntegerArray(InsertionSort(numbers));
-            Console.WriteLine("\n");
-        }
+        Console.WriteLine("Area: " + area);
+    }
+}
 
-        // Method implementing Insertion Sort algorithm
-        static int[] InsertionSort(int[] inputArray)
-        {
-            for (int i = 0; i < inputArray.Length - 1; i++)
-            {
-                for (int j = i + 1; j > 0; j--)
-                {
-                    // Swap if the element at j - 1 position is greater than the element at j position
-                    if (inputArray[j - 1] > inputArray[j])
-                    {
-                        int temp = inputArray[j - 1];
-                        inputArray[j - 1] = inputArray[j];
-                        inputArray[j] = temp;
-                    }
-                }
-            }
-            return inputArray; // Return the sorted array
-        }
-
-        // Method to print integer array elements
-        public static void PrintIntegerArray(int[] array)
-        {
-            foreach (int i in array)
-            {
-                Console.Write(i.ToString() + "  "); // Display each element followed by a space
-            }
-        }
+class Program
+{
+    static void Main()
+    {
+        Calculator c = new Calculator();
+        c.CalculateArea();
     }
 }
